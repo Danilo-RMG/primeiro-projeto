209e352f71d44aac61531bb467cb5937bc5e2ac0
Danilo-RMG <danilocfel@gmail.com>
2024-01-12 06:38:06 -0300

    terceiro commit

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("1");
+let nome = "Player";
+let xp = "1";
+let = "elo";
+
+if (xp <= 1000)
+ {
+  elo = "Ferro";
+ }
+ if (xp >= 1001 && xp <= 2000)
+ {
+  elo = "Bronze";
+ }
+ if (xp >= 2001 && xp <= 5000)
+ {
+  elo = "Prata";
+ }
+ if (xp >= 5001 && xp <= 7000)
+ {
+  elo = "Ouro";
+ }
+ if (xp >= 7001 && xp <= 8000)
+ {
+  elo = "Platina";
+ }
+ if (xp >= 8001 && xp <= 9000)
+ {
+  elo = "Ascendente";
+ }
+ if (xp >= 9001 && xp <= 10000)
+ {
+  elo = "Imortal";
+ }
+ if (xp >= 10001)
+ {
+  elo = "Radiante";
+ }
+
+ 
+console.log("O Herói de nome " + nome + " está no nível de " + elo);
