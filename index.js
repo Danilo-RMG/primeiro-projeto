1868c80df41b235d520db67768cc89c0cf1cbeba
Danilo-RMG <danilocfel@gmail.com>
2024-01-17 05:48:05 -0300

    quarto commit

--- index.js
+++ index.js
@@ -9,33 +9,33 @@ if (xp <= 1000)
  {
   elo = "Ferro";
  }
- if (xp >= 1001 && xp <= 2000)
- {
-  elo = "Bronze";
- }
- if (xp >= 2001 && xp <= 5000)
- {
-  elo = "Prata";
- }
- if (xp >= 5001 && xp <= 7000)
- {
-  elo = "Ouro";
- }
- if (xp >= 7001 && xp <= 8000)
- {
-  elo = "Platina";
- }
- if (xp >= 8001 && xp <= 9000)
- {
-  elo = "Ascendente";
- }
- if (xp >= 9001 && xp <= 10000)
- {
-  elo = "Imortal";
- }
- if (xp >= 10001)
- {
-  elo = "Radiante";
+  else if (xp >= 1001 && xp <= 2000)
+   {
+    elo = "Bronze";
+   }
+    else if (xp >= 2001 && xp <= 5000)
+     {
+      elo = "Prata";
+     }
+      else if (xp >= 5001 && xp <= 7000)
+       {
+        elo = "Ouro";
+       }
+        else if (xp >= 7001 && xp <= 8000)
+         {  
+          elo = "Platina";
+         }
+          else if (xp >= 8001 && xp <= 9000)
+           {
+            elo = "Ascendente";
+           }
+            else if (xp >= 9001 && xp <= 10000)
+             {
+              elo = "Imortal";
+             }
+              else  (xp >= 10001)
+               {
+                elo = "Radiante";
  }
 
 
